5b2f7f3be746dccf9324da6cfa492fd240b3abbc
Josh Wymer <josh@mixpanel.com>
2016-05-10 23:59:08 +0000

    unify build paths

--- rollup.config.cjs.js
+++ rollup.config.cjs.js
@@ -3,7 +3,7 @@ import babel from 'rollup-plugin-babel';
 export default {
   format: 'cjs',
   entry: './src/index.js',
-  dest: './dist/index.cjs.js',
+  dest: './build/index.cjs.js',
   plugins: [
     babel({
       exclude: 'node_modules/**'
--- rollup.config.es6.js
+++ rollup.config.es6.js
@@ -3,7 +3,7 @@ import babel from 'rollup-plugin-babel';
 export default {
   format: 'es6',
   entry: 'src/index.js',
-  dest: 'dist/index.es6.js',
+  dest: 'build/index.es6.js',
   plugins: [
     babel({
       exclude: 'node_modules/**'
